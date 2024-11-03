3eeb8bd324e84b45ddfe5296b02e338ebacf55c5
Igor Bazanov <vsyakiepysma@gmail.com>
2024-11-03 13:53:29 +0700

    Есть ошибка

--- lessons11-20/lesson18_practice.py
+++ lessons11-20/lesson18_practice.py
@@ -1,5 +1,5 @@
 # Цикл for - Практика
-
+from itertools import count
 
 # Задание
 # Напишите программу, которая нарисует в консоли ромбик из звездочек c одинаковыми по длине сторонами.
@@ -44,3 +44,31 @@ size = input("Введите размер ромбика: ")
 print("Размер должен быть нечетным числом от 3 до 15: 7\nВаш ромбик:")
 if not size.isdigit() or 3<int(size)>=15 or int(size)%2 == 0:
     print("Некорректное значение!")
+# else:
+#     for a1 in range(1, (int(size)+1)//2 + 1): #from row 1 to 5
+#         for a2 in range((int(size)+1)//2 - a1):
+#             print(" ", end = "")
+#         for a3 in range((a1*2)-1):
+#             print("*", end = "")
+#         print()
+#
+#     for a1 in range((int(size)+1)//2 + 1,int(size) + 1): #from row 6 to 9
+#         for a2 in range(a1 - (int(size)+1)//2):
+#             print(" ", end = "")
+#         for a3 in range((int(size)+1 - a1)*2 - 1):
+#             print("*", end = "")
+#         print()
+min_size =3
+count1 =1
+count2 =1
+for i in range(min_size,int(size)):
+    for j in (min_size,size):
+        print(" "*(min_size+count1)+"*"*count2)
+        count1-=1
+        count2+=2
+    if count2 ==int(size):
+        for k in range(min_size, int(size)):
+            for l in (min_size, size):
+                print(" " * (min_size + count1) + "*" * count2)
+                count1 += 1
+                count2 -= 2
